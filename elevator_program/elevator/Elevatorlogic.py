db5e05213b8db0d2f156bc5ae578f9abab04926b
HarshTiwari1710 <harshtiwari1711@gmail.com>
2023-05-31 17:57:14 +0530

    Thus is database migration  commit

--- elevator_program/elevator/Elevatorlogic.py
+++ elevator_program/elevator/Elevatorlogic.py
@@ -1,55 +1,52 @@
+from collections import deque
+
 UP_Button = 1
 DOWN_Button = 2
 FLOOR_COUNT = 6
 
 class ElevatorLogic(object):
-    """
-    To interact with the world, you can get the current floor from the
-    `current_floor` property of the `callbacks` object, and you can move the
-    elevator by setting the `motor_direction` property. See below for how this is done.
-    """
-
     def __init__(self):
-        # Feel free to add any instance variables you want.
         self.destination_floor = None
         self.callbacks = None
+        self.requests = deque()  # Queue to store elevator requests
 
     def on_called(self, floor, direction):
-        """
-        This is called when somebody presses the up or down button to call the elevator.
-        This could happen at any time, whether or not the elevator is moving.
-        The elevator could be requested at any floor at any time, going in either direction.
+        self.requests.append((floor, direction))  # Add the new request to the queue
 
-        floor: the floor that the elevator is being called to
-        direction: the direction the caller wants to go, up or down
-        """
-        self.destination_floor = floor
+        # If the elevator is not moving, process the request immediately
+        if self.callbacks.motor_direction is None:
+            self.process_request()
 
     def on_floor_selected(self, floor):
-        """
-        This is called when somebody on the elevator chooses a floor.
-        This could happen at any time, whether or not the elevator is moving.
-        Any floor could be requested at any time.
+        self.requests.append((floor, None))  # Add the new destination floor to the queue
 
-        floor: the floor that was requested
-        """
-        self.destination_floor = floor
+        # If the elevator is not moving, process the request immediately
+        if self.callbacks.motor_direction is None:
+            self.process_request()
 
     def on_floor_changed(self):
-        """
-        This lets you know that the elevator has moved one floor up or down.
-        You should decide whether or not you want to stop the elevator.
-        """
         if self.destination_floor == self.callbacks.current_floor:
             self.callbacks.motor_direction = None
 
+            # Process the next request in the queue
+            if self.requests:
+                self.process_request()
+
     def on_ready(self):
-        """
-        This is called when the elevator is ready to go.
-        Maybe passengers have embarked and disembarked. The doors are closed,
-        time to actually move, if necessary.
-        """
-        if self.destination_floor > self.callbacks.current_floor:
+        # If there are pending requests, process the next request in the queue
+        if self.requests:
+            self.process_request()
+
+    def process_request(self):
+        # Pop the next request from the queue
+        floor, direction = self.requests.popleft()
+
+        # Determine the motor direction based on the request
+        if floor > self.callbacks.current_floor:
             self.callbacks.motor_direction = UP_Button
-        elif self.destination_floor < self.callbacks.current_floor:
+        elif floor < self.callbacks.current_floor:
             self.callbacks.motor_direction = DOWN_Button
+        else:
+            self.callbacks.motor_direction = None
+
+        self.destination_floor = floor
